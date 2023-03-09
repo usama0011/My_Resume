177954f30e51453e26df9a78da6de9e7287e8432
usama0011 <za5232208#gmail.com>
2023-03-09 16:34:02 +0500

    update commit

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,8 +1,9 @@
 import styles from '../styles/Footer.module.css'
-export const Footer = () => {
+ const Footer = () => {
   return (
     <div className={styles.container}>
       <p>© 2022 by 𝓤𝓼𝓪𝓶𝓪 𝓐𝓱𝓶𝓪𝓭 All rights reserved.</p>
     </div>
   )
 }
+export default Footer;
--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,12 +1,10 @@
-import { Footer } from "./Footer"
 import Header from "./Header"
-
+import Footer from '../components/Footer';
 const Layout = ({children}) => {
   return (
   <>
   <Header/>
   {children}
-  <Footer/>
   
   </>
   )
